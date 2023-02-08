e269290dc7a9ab025082b8d17b9d025aba27e403
Bhartesh Kaushik <bharteshkaushik@gmail.com>
2023-02-08 23:49:45 +0530

    Restructure

---
+++ src/Pages/AddTaskDetailModal.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
+export const AddTaskDetailModal = () => {
+  return (
+    <div>
+      <input type="text"></input>
+    </div>
+  );
+};
